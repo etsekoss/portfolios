a9799f4d9bdf5e3dc01e3c201c88d0071ee4b10a
Kossivi <kossivi.etse@outlook.fr>
2025-12-31 13:19:48 +0100

    Serve media files in production

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings  # Import pour les settings
-from django.conf.urls.static import static  # Pour les fichiers statiques
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('projects.urls')),  # Inclure les URLs de l'application 'projects'
+    path("admin/", admin.site.urls),
+    path("", include("projects.urls")),
 ]
 
-# Ajouter cette ligne pour servir les fichiers médias
-if settings.DEBUG:  # Seulement en mode développement
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Servir les fichiers media (Render / prod inclus)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
